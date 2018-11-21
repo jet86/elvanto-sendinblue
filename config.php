309dab7f1d8ce334d276607b8a09aa79d0e9945e
jet86 <tim@timjones.id.au>
2018-11-21 20:19:00 +1100

    Created initial config file template

---
+++ config.php
@@ -0,0 +1,11 @@
+<?PHP
+	$elvantoAPIKey = "";
+	$sendinblueAPIKey = "";
+
+	// Set the authentication mode for accessing the sync page
+	// 0 = No authentication required
+	// 1 = Local password required
+	// 2 = Local Nextcloud instance account auth (not yet implemented)
+	// 3 = External account auth (not yet implemented)
+	$authMode = 0;
+?>
