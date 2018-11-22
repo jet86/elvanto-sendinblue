f8aea7c5974023f7e431a6ba7a5c9bc2c328ac31
Tim Jones <tim@timjones.id.au>
2018-11-22 11:28:32 +1100

    Test for inclusion of config file

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?PHP
-	include ("config.php");
-	echo "Hello, world.";
+	if include("local_config.php")
+	{
+		echo "Local config file included.";
+	}
+	elseif include("config.php")
+	{
+		echo "Config file included.";
+	}
+	else
+	{
+		echo "Config file not found.";
+	}
 ?>
