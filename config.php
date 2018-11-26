80ba289e5d9a3d90a0349a6e9f80dfa19c6f173f
Tim Jones <tim@timjones.id.au>
2018-11-26 20:41:23 +1100

    Add elvantoGroupIDs array

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 	$elvantoRedirectURI = "";
 	
 	// Elvanto Configuration Parameters
-	//
+	// $elvantoGroupIDs = array('1', '2', '3'); // Replace with the Group IDs (GUIDS) from Elvanto you wish to sync
 	
 	// Send In Blue Connection Parameters
 	$sendinblueAPIKey = "";
