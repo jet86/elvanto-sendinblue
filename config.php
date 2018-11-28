a43266eb5d9d2e81034aad22efd1f79b1c2ac4aa
Tim Jones <tim@timjones.id.au>
2018-11-28 16:34:32 +1100

    Added config file version

--- config.php
+++ config.php
@@ -1,21 +1,34 @@
 <?PHP
 	// Administrator Details
 	// Fill in the contact name and email and/or phone for the person managing this installation and config
+
 	$adminName = "";
 	$adminEmail = "";
 	$adminPhone = "";
 
 	// Elvanto Connection Parameters
+
 	$elvantoClientID = "";
 	$elvantoClientSecret = "";
 	$elvantoRedirectURI = "";
 	
 	// Elvanto Configuration Parameters
-	// $elvantoGroupIDs = array('1', '2', '3'); // Replace with the Group IDs (GUIDS) from Elvanto you wish to sync
+
+	// Uncomment the following line and replace with the Group IDs (GUIDS) from Elvanto which you wish to sync:
+	// $elvantoGroupIDs = array('1', '2', '3');
 	
 	// Send In Blue Connection Parameters
+
 	$sendinblueAPIKey = "";
 	
 	// Send In Blue Configuration Parameters
+
 	//
+
+	/* ----- Config file version ----- */
+	//
+	// Do not change the following value unless you are sure you need to
+	// and you have included all required configuration paramaters
+	//
+	$configVersion = "1";
 ?>
