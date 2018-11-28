68ee4fae0b5aa8c4a1574fa1f6575d0aea102c41
Tim Jones <tim@timjones.id.au>
2018-11-28 15:58:37 +1100

    Removed redundant AuthMode

--- config.php
+++ config.php
@@ -12,11 +12,4 @@
 	
 	// Send In Blue Configuration Parameters
 	//
-
-	// Set the authentication mode for accessing the sync page
-	// 0 = No authentication required
-	// 1 = Local password required
-	// 2 = Local Nextcloud instance account auth (not yet implemented)
-	// 3 = External account auth (not yet implemented)
-	$authMode = 0;
 ?>
